b7b8cb7d1c87ecfe1c56a1b7ebacc088ab68b4f5
Tao-Yu <taoxyu@gmail.com>
2018-06-19 21:37:14 +0100

    Add three scramble modes

---
+++ js/parityScrambler.js
@@ -0,0 +1,61 @@
+
+scramblers["333"].initialize()
+function get333scramble(){
+    return scramblers["333"].getRandomScramble().scramble_string;
+}
+
+function parity(scramble){
+    var qtm = alg.cube.countMoves(scramble, {metric:"obqtm"})
+    return qtm % 2 == 0? false : true;
+}
+
+function changeParity(scramble){
+    scramble = scramble.trim();
+    
+    switch (scramble[scramble.length - 1]){
+        case "'":
+        case "2":
+            return scramble.substring(0, scramble.length-1);
+        default:
+            return scramble + "'";
+    }
+}
+
+function generateScrambles(amount, mode){
+    var scrambles = [];
+    switch (mode){
+        case 'forceParity':
+            while (scrambles.length < amount){
+                var scramble = get333scramble();
+                if (parity(scramble)){
+                    scrambles.push(scramble);
+                }
+            }
+        case 'forceNoParity':
+            while (scrambles.length < amount){
+                var scramble = get333scramble();
+                if (!parity(scramble)){
+                    scrambles.push(scramble);
+                }
+            }
+        case 'indicate':
+            while (scrambles.length < amount){
+                var scramble = get333scramble();
+                if (parity(scramble)){
+                    scrambles.push("(p) " + scramble);
+                }
+                else {
+                    scrambles.push("(np) " + scramble);
+                }
+            }
+    }
+    return scrambles;
+}
+
+function displayScrambles(){
+    var amount = parseInt(document.getElementById("amount").value);
+    var e = document.getElementById("mode");
+    var mode = e.options[e.selectedIndex].value;
+    
+    var scrambles = generateScrambles(amount, mode).join("\n");
+}
